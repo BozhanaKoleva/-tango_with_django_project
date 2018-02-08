772110f7fb862a290491d34c58d7a4d6e168d340
BozhanaKoleva <35367937+BozhanaKoleva@users.noreply.github.com>
2018-02-08 16:23:24 +0000

    Chapter 8

--- rango/views.py
+++ rango/views.py
@@ -1,9 +1,43 @@
 from django.shortcuts import render
+from django.shortcuts import redirect
+from django.core.urlresolvers import reverse
 from django.http import HttpResponse
-from rango.models import Category
-from rango.models import Page
-from rango.forms import CategoryForm
-from rango.forms import PageForm
+from rango.models import Category, Page
+from rango.forms import CategoryForm, PageForm
+from datetime import datetime
+
+
+# Create your views here.
+def get_server_side_cookie(request, cookie, default_val=None):
+    val = request.session.get(cookie)
+    if not val:
+        val = default_val
+    return val
+
+def visitor_cookie_handler(request):
+    # Get the number of visits to the site.
+    # We use the COOKIES.get() function to obtain the visits cookie.
+    # If the cookie exists, the value returned is casted to an integer.
+    # If the cookie doesn't exist, then the default value of 1 is used.
+    visits = int(get_server_side_cookie(request, 'visits', '1'))
+
+    last_visit_cookie = get_server_side_cookie(request, 'last_visit', str(datetime.now()) )
+
+    last_visit_time = datetime.strptime(last_visit_cookie[:-7], "%Y-%m-%d %H:%M:%S")
+    #last_visit_time = datetime.now()
+    # If it's been more than a day since the last visit...
+    if (datetime.now() - last_visit_time).seconds > 0:
+        visits = visits + 1
+        #update the last visit cookie now that we have updated the count
+        request.session['last_visit'] = str(datetime.now())
+    else:
+        visits = 1
+        # set the last visit cookie 
+        request.session['last_visit'] = last_visit_cookie
+    # update/set the visits cookie
+    request.session['visits'] = visits
+
+
 
 
 def index(request):
@@ -26,36 +60,67 @@ def index(request):
     response = render(request, 'rango/index.html', context=context_dict)
     
     return response
-
+    
 
 def about(request):
-    context_dict = {'boldmessage': "This tutorial has been put together by Bozhana Koleva"}
-    return render(request, 'rango/about.html', context=context_dict)
-
+    if request.session.test_cookie_worked():
+        print("TEST COOKIE WORKED!")
+        request.session.delete_test_cookie()
+    # To complete the exercise in chapter 4, we need to remove the following line
+    # return HttpResponse("Rango says here is the about page. <a href='/rango/'>View index page</a>")
+    
+    # and replace it with a pointer to ther about.html template using the render method
+    return render(request, 'rango/about.html',{})
+    
 def show_category(request, category_name_slug):
     # Create a context dictionary which we can pass
     # to the template rendering engine.
     context_dict = {}
+    
     try:
         # Can we find a category name slug with the given name?
         # If we can't, the .get() method raises a DoesNotExist exception.
         # So the .get() method returns one model instance or raises an exception.
         category = Category.objects.get(slug=category_name_slug)
         # Retrieve all of the associated pages.
-        # Note that filter() will return a list of page objects or an empty list
+        # Note that filter() returns a list of page objects or an empty list
         pages = Page.objects.filter(category=category)
+        
         # Adds our results list to the template context under name pages.
         context_dict['pages'] = pages
         # We also add the category object from
         # the database to the context dictionary.
         # We'll use this in the template to verify that the category exists.
         context_dict['category'] = category
-    except Category.DoesNotExist:
+
         # We get here if we didn't find the specified category.
+        # Don't do anything -
+        # the template will display the "no category" message for us.        
+    except Category.DoesNotExist:
         context_dict['category'] = None
         context_dict['pages'] = None
-    return render(request, 'rango/category.html', context_dict)
 
+
+    # create a default query based on the category name
+    # to be shown in the search box
+    context_dict['query'] = category.name
+
+    result_list = []
+    if request.method == 'POST':
+        query = request.POST['query'].strip()
+        if query:
+            # Run our Webhose function to get the results list!
+            result_list = run_query(query)
+            context_dict['query'] = query
+    context_dict['result_list'] = result_list
+
+
+    # Go render the response and return it to the client.
+    return render(request, 'rango/category.html', context_dict)
+    
+    
+    
+    
 def add_category(request):
     form = CategoryForm()
     # A HTTP POST?
@@ -64,20 +129,27 @@ def add_category(request):
         # Have we been provided with a valid form?
         if form.is_valid():
             # Save the new category to the database.
-            form.save(commit=True)
+            category = form.save(commit=True)
+            print(category, category.slug)
+            # Now that the category is saved
+            # We could give a confirmation message
+            # But instead since the most recent catergory added is on the index page
+            # Then we can direct the user back to the index page.
             return index(request)
         else:
-            # The supplied form contained errors -
-            # just print them to the terminal.
-
+            # The supplied form contained errors - just print them to the terminal.
             print(form.errors)
-
+    # Will handle the bad form (or form details), new form or no form supplied cases.
+    # Render the form with error messages (if any).
     return render(request, 'rango/add_category.html', {'form': form})
+    
+    
 def add_page(request, category_name_slug):
     try:
         category = Category.objects.get(slug=category_name_slug)
     except Category.DoesNotExist:
         category = None
+
     form = PageForm()
     if request.method == 'POST':
         form = PageForm(request.POST)
@@ -87,9 +159,39 @@ def add_page(request, category_name_slug):
                 page.category = category
                 page.views = 0
                 page.save()
-                return show_category(request, category_name_slug)
+                # probably better to use a redirect here.
+            return show_category(request, category_name_slug)
         else:
             print(form.errors)
+
     context_dict = {'form':form, 'category': category}
+
     return render(request, 'rango/add_page.html', context_dict)
+    
+    
+def search(request):
+    result_list = []
+    if request.method == 'POST':
+        query = request.POST['query'].strip()
+        if query:
+             # Run our Webhose function to get the results list!
+             result_list = run_query(query)
+    return render(request, 'rango/search.html', {'result_list': result_list})
+    
+    
+def track_url(request):
+    page_id = None
+    if request.method == 'GET':
+        if 'page_id' in request.GET:
+            page_id = request.GET['page_id']
+    if page_id:
+        try:
+            page = Page.objects.get(id=page_id)
+            page.views = page.views + 1
+            page.save()
+            return redirect(page.url)
+        except:
+            return HttpResponse("Page id {0} not found".format(page_id))
+    print("No page_id in get string")
+    return redirect(reverse('index'))
 
